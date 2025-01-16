b02c9af155c30c41485ac35fb5a52d0c2b6d9c7f
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-01-16 12:26:26 +0100

    work in progress

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ notes_asc = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "H","C"
 notes_desc = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "B", "H"];
 let scale_tonic = String(document.getElementById("original_scale").value);
 //let prog = prompt("Chord Progression");
-progArray = ['C', 'D', 'E', 'F']; /*prog.split("-");*/
+let progArray;
 original_scale = [];
 modified_scale = [];
 modified_prog = [];
 progression = [];
 
-scaleFinder(scale_tonic, original_scale);
+
 function scaleFinder(x, y) {
     for(i = 0; i<notes_asc.length; i++) {
         if(notes_asc[i] == x) {
@@ -27,7 +27,7 @@ function scaleFinder(x, y) {
     }
 }
 
-degFinder();
+
 function degFinder() {
     for(i = 0; i < progArray.length; i++) {
         for(j = 0; j < original_scale.length; j++) {
@@ -47,11 +47,26 @@ function progFinder() {
 
 
 function convert() {
+    progArray = document.getElementById("prog_notes").value.split("-");
     let new_scale = String(document.getElementById("new_scale").value);
     scaleFinder(new_scale, modified_scale);
     progFinder();
     console.log(modified_scale);
     console.log(modified_prog);
+}
+
+let checkbox = false;
+document.getElementById("prog_degrees").style = "display: none;";
+function showCheckboxValue() {
+    if(checkbox) {
+        checkbox = false;
+        document.getElementById("prog_degrees").style = "display: none;";
+        document.getElementById("prog_notes").style = "display: inline;";
+    } else {
+        checkbox = true;
+        document.getElementById("prog_degrees").style = "display: inline";
+        document.getElementById("prog_notes").style = "display: none";
+    }
 }
 
 console.log(progArray);
