7ec4a1c666430d7dc77adc14c6c891c7f0a03b06
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-01-24 16:59:49 +0100

    finished js now doing design and outputing

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-degree = [];
+document.getElementById("prog_degrees").style = "display: none;";
 steps = [1,1,0,1,1,1,0];
 notes_asc = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "H","C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "H"];
 notes_desc = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "B", "H"];
-let scale_tonic = String(document.getElementById("original_scale").value);
-//let prog = prompt("Chord Progression");
-let progArray;
+degree = [];
 original_scale = [];
+original_prog = [];
 modified_scale = [];
 modified_prog = [];
-progression = [];
+let original_scale_tonic;
+let modified_scale_tonic;
+let checkbox = false;
 
-
-function scaleFinder(x, y) {
+function scaleFinder(tonic, scale) { //Finds the scale from the original tonic and fills it into scale
     for(i = 0; i<notes_asc.length; i++) {
-        if(notes_asc[i] == x) {
-            y.push(notes_asc[i]);
+        if(notes_asc[i] == tonic) {
+            scale.push(notes_asc[i]);
             for(j = 0; j<steps.length-1; j++) {
                 if(steps[j] == 1) {
-                    y.push(notes_asc[notes_asc.indexOf(y[y.length-1])+2]);
+                    scale.push(notes_asc[notes_asc.indexOf(scale[scale.length-1])+2]);
                 } else if(steps[j] == 0) {
-                    y.push(notes_asc[notes_asc.indexOf(y[y.length-1])+1]);
+                    scale.push(notes_asc[notes_asc.indexOf(scale[scale.length-1])+1]);
                 }
             }
             break;
@@ -27,37 +27,45 @@ function scaleFinder(x, y) {
     }
 }
 
-
-function degFinder() {
-    for(i = 0; i < progArray.length; i++) {
+function degFinder(prog) { //Finds the degrees of the given progression 'prog' and fills it into degree
+    for(i = 0; i < prog.length; i++) {
         for(j = 0; j < original_scale.length; j++) {
-            if(progArray[i] == original_scale[j]) {
+            if(prog[i] == original_scale[j]) {
                 degree.push(j+1);
             }
         }
     }
 }
 
-function progFinder() {
+function progFinder(scale, prog) { //Finds the progression of the given degrees in the given scale 'scale' and fills it into 'prog'
     for(i = 0; i < degree.length; i++) {
-        modified_prog.push(modified_scale[degree[i]-1]);
+        prog.push(scale[degree[i]-1]);
     }
 }
 
-
-
 function convert() {
-    progArray = document.getElementById("prog_notes").value.split("-");
-    let new_scale = String(document.getElementById("new_scale").value);
-    scaleFinder(new_scale, modified_scale);
-    progFinder();
-    console.log(modified_scale);
-    console.log(modified_prog);
+    original_scale_tonic = String(document.getElementById("original_tonic").value);
+    modified_scale_tonic = String(document.getElementById("new_tonic").value);
+    scaleFinder(original_scale_tonic, original_scale); console.log("original_scale = "+original_scale);
+    document.getElementById("original_scale").value = original_scale;
+    scaleFinder(modified_scale_tonic, modified_scale); console.log("modified_scale = "+modified_scale);
+    document.getElementById("new_scale").value = modified_scale;
+    if(!checkbox) {
+        original_prog = document.getElementById("prog_notes").value.split("-"); console.log("original_prog = "+original_prog);
+        degFinder(original_prog); console.log("degree = "+degree);
+        document.getElementById("p_degrees").value = degree;
+        progFinder(modified_scale, modified_prog); console.log("modified_prog = "+modified_prog);
+        document.getElementById("p_modified_prog").value = modified_prog;
+    } else {
+        degree = document.getElementById("prog_degrees").value.split("-"); console.log("degree = "+degree);
+        document.getElementById("p_degrees").value = degree;
+        progFinder(original_scale, original_prog); console.log("original_prog = "+original_prog);
+        progFinder(modified_scale, modified_prog); console.log("modified_prog = "+modified_prog);
+        document.getElementById("p_modified_progression").value = modified_prog;
+    }
 }
 
-let checkbox = false;
-document.getElementById("prog_degrees").style = "display: none;";
-function showCheckboxValue() {
+function changeCheckbox() {
     if(checkbox) {
         checkbox = false;
         document.getElementById("prog_degrees").style = "display: none;";
@@ -68,8 +76,3 @@ function showCheckboxValue() {
         document.getElementById("prog_notes").style = "display: none";
     }
 }
-
-console.log(progArray);
-console.log(original_scale);
-console.log(degree);
-console.log(original_scale[original_scale.length-1]);
