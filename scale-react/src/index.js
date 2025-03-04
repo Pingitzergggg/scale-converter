1a3831e3fd252e243168d2b894d1f71313d5217b
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-03-04 21:58:58 +0100

    added alert message with react

--- scale-react/src/index.js
+++ scale-react/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './alert.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('message-01'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div style={{textAlign: "center"}} className="alert-center">
+      <h3>Title</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus corporis nostrum id ab repellendus, vero aliquam nulla error eligendi. Necessitatibus officia architecto culpa id laudantium ipsam fugit expedita a!</p>
+      <a className="alert-button">Next</a>
+    </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
