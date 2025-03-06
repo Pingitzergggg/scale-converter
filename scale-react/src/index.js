ce4a8253a520b3f00adf4789dd8601c763ae6bb3
Pingitzer Dávid <davidpingitzer7@gmail.com>
2025-03-06 12:28:05 +0100

    made popups

--- scale-react/src/index.js
+++ scale-react/src/index.js
@@ -5,20 +5,32 @@ import './alert.css';
 
 const root = ReactDOM.createRoot(document.getElementById('message'));
 
-let text = [];
-let title = [];
+let text = ["text1", "text2"];
+let title = ["title1", "title2"];
 
-let level = 0 /*(localStorage.getItem('tutorial') == 'true') ? 1 : 0*/;
+let isRunnable = true; console.log(isRunnable)
+let level = 0; /*(localStorage.getItem('tutorial') == 'true') ? 1 : 0*/
 
 function setStyleSheet(pos) { /*position of the div*/
-  return (
-    {
-      height: "100vh",
-      display: "flex",
-      justifyContent: pos,
-      alignItems: "flex-end"
-    }
-  )
+  if (level == 0) {
+    return (
+      {
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }
+    )
+  } else {
+    return (
+      {
+        height: "100vh",
+        display: "flex",
+        justifyContent: pos,
+        alignItems: "flex-end"
+      }
+    )
+  }
 }
 
 function highLighter(x) {
@@ -26,21 +38,52 @@ function highLighter(x) {
   element.style = "transition: 2s; border-radius: none; border-color: black; box-shadow: none; outline: 0 none;";
 }
 
+function increaseLevel() {
+  if (level < 5) {
+    level++;
+  }
+  if (level == 1) {
+    root.render(Message("flex-start", level));
+  } else if (level == 2) {
+    root.render(Message("center", level));
+  } else if (level == 3) {
+    root.render(Message("center", level))
+  } else if (level == 4) {
+    root.render(Message("flex-end", level));
+  } else {
+    isRunnable = false; //localstorage update
+    root.render(<></>);
+  }
+  console.log(level);
+}
+
 function Message(pos, level) {
+  return (
   <div className='container'>
     <div className='row' style={setStyleSheet(pos)}>
       <div className='col-md-4'>
         <div style={{textAlign: "center"}} className="alert-center">
-          <h3>Title</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus corporis nostrum id ab repellendus, vero aliquam nulla error eligendi. Necessitatibus officia architecto culpa id laudantium ipsam fugit expedita a!</p>
-          <a className="alert-button">Next</a>
+          <div style={{display: "flex", justifyContent: "flex-end"}}>
+          <a onClick={() => {level = 5; root.render(<></>)}} className='cancel'><i class="fa-solid fa-xmark"></i></a>
+          </div>
+          <h3>{title[level]}</h3>
+          <p>{text[level]}</p>
+          <a onClick={increaseLevel} className="alert-button">Next</a>
         </div>
       </div>
     </div>
   </div>
+  )
 }
 
-root.render();
+
+// if (isRunnable) {
+//   root.render(Message("center", level));
+// } else {
+//   root.render(<></>);
+// }
+
+root.render(Message("center", level));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
